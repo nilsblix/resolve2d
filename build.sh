ce369ee221b18eb0bf600d147335c7722452d7ec
nilsblix <nilsblix06@gmail.com>
2025-05-21 19:51:27 +0200

    updated build.sh to reflect web

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "BUILD: Compiling zig..:"
 zig build
 cp -a zig-out/bin/zigics.wasm webdemo/public
 cp zig-out/bin/zigics.wasm webdemo/dist
+cd webdemo
+echo "BUILD: Compiling typescript/webdemo"
+npm run build
